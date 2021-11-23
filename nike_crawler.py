f0317baf991637f179908510a004201498c4009c
JavierSantos <javier.santos.sanchez@alumnos.upm.es>
2021-11-23 12:39:29 +0100

    Concurrencia para el bot de Stockx

--- nike_crawler.py
+++ nike_crawler.py
@@ -1,21 +1,15 @@
-import requests
 from fpdf import FPDF
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Firefox
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import logging
 import json
-import _thread as thread
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import time
-from threading import Thread
-from multiprocessing import Queue, cpu_count
 from PIL import Image
 from concurrent import futures
 
